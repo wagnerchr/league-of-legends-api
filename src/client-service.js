e84473e09dacd97b35d53d8aa6930ef31350f42a
WagnerChr <wagnerckv@gmail.com>
2023-06-10 10:50:15 -0300

    refactor: Change functions to async functions

--- src/client-service.js
+++ src/client-service.js
@@ -1,111 +1,67 @@
-let mainTag = document.querySelector('main');
+// Configs
+const apiUrl = 'https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json';
+// const jsonServerUrl = 'http://localhost:3000/champions';
 
-fetch(`https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json`)
-  .then(response => response.json())
-  .then(data => {
-
-    const filteredChampions = Object.values(data.data).filter(champion => {
-      console.log(champion.name.toLowerCase().startsWith("A"));
-    });
-
-
-    const champions = data.data;
-    for (const championKey in champions) {
-
-      const champion = champions[championKey];
-      const championName = champion.id;
-
-
-
-      const championIcon = champion.image.full
-      const championImg = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${championIcon}`;
-
-      // Main Space
-      const divElement = document.createElement('div');
-
-      divElement.classList.add("card");
-
-      // Card
-      divElement.innerHTML = `
-      <div class="card">
-        <h3 class="card">${championName}</h3>
-        <img class="card" src="${championImg}"/>
-      </div>
-      `
-
-      mainTag.appendChild(divElement)
-      
-    }
-  })
-  .catch(error => {
-    console.error('Erro:', error);
-  });
-
-
-  const search = (value) => {
-
+const fetchChampions = async (value) => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const champions = Object.values(data.data);
+    let mainTag = document.querySelector('main');
     mainTag.innerHTML = ''
-
-  fetch(`https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json`)
-  .then(response => response.json())
-  .then(data => {
-
-    const filteredChampions = Object.values(data.data).filter(champion => {
-      console.log(champion.name.toLowerCase().startsWith("A"));
-    });
-
-
-    const champions = data.data;
-    for (const championKey in champions) {
-
-      const champion = champions[championKey];
+    
+    champions.forEach(champion => {
       const championName = champion.id;
+      const championIcon = champion.image.full;
 
-      if(championName.toLowerCase().startsWith(value.toLowerCase())) {
+    if(championName.toLowerCase().startsWith(value.toLowerCase())) {
 
-      const championIcon = champion.image.full
       const championImg = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${championIcon}`;
 
-      // Main Space
       const divElement = document.createElement('div');
-
       divElement.classList.add("card");
 
-      // Card
       divElement.innerHTML = `
-      <div class="card">
-        <h3 class="card">${championName}</h3>
-        <img class="card" src="${championImg}"/>
-      </div>
-      `
-
-      mainTag.appendChild(divElement)
-      }
-    }
-
-  })
-  .catch(error => {
+        <div class="card">
+          <h3 class="card">${championName}</h3>
+          <img class="card" src="${championImg}"/>
+        </div>
+      `;
+      mainTag.appendChild(divElement);
+    }});
+  } catch (error) {
     console.error('Erro:', error);
-  });
   }
+};
+
+const search = async (value) => {
+  await fetchChampions(value);
+}
 
 const send = () => {
 
-  var inputName = document.getElementById('input-name');
-  var inputImg = document.getElementById('input-img');
+  const inputName = document.getElementById('input-name');
+  const inputImg = document.getElementById('input-img');
 
-  var valueName = inputName.value;
-  var valueImg = inputImg.value;
+  const valueName = inputName.value,
+        valueImg = inputImg.value;
 
   const divElement = document.createElement('div');
   divElement.innerHTML = `
-  <div class="card">
-    <h3 class="card">${valueName}</h3>
-    <img class="card" src="${valueImg}"/>
-  </div>
+    <div class="card">
+      <h3 class="card">${valueName}</h3>
+      <img class="card" src="${valueImg}"/>
+    </div>
   `
+  console.log(
+    valueName +
+    "\n- - -\n" +
+    valueImg
+  )
 
-  mainTag.appendChild(divElement)
-
-  
+  // Problem
+  // mainTag.appendChild(divElement)
 }
+
+// Starting with Default
+fetchChampions('');
