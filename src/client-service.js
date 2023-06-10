1368bd7dbc5868a099584b415e3cc8e0c194e30a
WagnerChr <wagnerckv@gmail.com>
2023-06-10 16:25:09 -0300

    feat: Add edit model display

--- src/client-service.js
+++ src/client-service.js
@@ -11,20 +11,45 @@ const fetchIsso = async () => {
   champions.forEach(champion => {
       const championName = champion.id;
       const championIcon = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${champion.image.full}`;    
+      
+      let key = localStorage.length + 1;
+
       const champ = {
+        key: key,
         name: championName,
         img: championIcon
       };
-
-      let key = localStorage.length + 1;
       localStorage.setItem( key, JSON.stringify(champ));
   });
 }
 
+const edit = (champ) => { 
+  var modal = document.getElementsByClassName("modal")[0];
+  modal.style.display = "flex";
+
+  const champFromLocalStorage = JSON.parse(localStorage.getItem(champ));
+
+  let inputName = document.querySelector(".input-edit-name")
+  inputName.value = champFromLocalStorage.name;
+  let inputImage = document.querySelector(".input-edit-img")
+  inputImage.value = champFromLocalStorage.img;
+  let championImage = document.querySelector(".champion-img")
+  let imgElement = document.createElement("img"); 
+  imgElement.src=`${champFromLocalStorage.img}` 
+  championImage.appendChild(imgElement)
+
+  const closeBtn = document.querySelector(".close");
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = "none";
+    championImage.innerHTML = ''
+  });
+}
+
+
 // Inserts Champions
 const insertChamp = (value) => {
   mainTag.innerHTML = ''
-  for (let i = 1; i < localStorage.length; i++) {
+  for (let i = 1; i < localStorage.length + 1; i++) {
     champFromLocalStorage = JSON.parse(localStorage.getItem(i));
 
   if(champFromLocalStorage.name.toLowerCase().startsWith(value.toLowerCase())) {
@@ -35,55 +60,16 @@ const insertChamp = (value) => {
       <div class="card">
         <h3 class="card">${champFromLocalStorage.name}</h3>
         <img class="card" src="${champFromLocalStorage.img}"/>
+        <button class="editButton">Edit</button>
       </div>
     `;
+
+    const editButton = divElement.querySelector(".editButton");
+    editButton.addEventListener("click", edit.bind(null, i));
+
     mainTag.appendChild(divElement);    
   }}
 }
-
-
-// const fetchChampions = async (value) => {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     const champions = Object.values(data.data);
-    
-//     mainTag.innerHTML = ''
-    
-//     champions.forEach(champion => {
-//       const championName = champion.id;
-//       const championIcon = champion.image.full;
-
-//     if(championName.toLowerCase().startsWith(value.toLowerCase())) {
-
-//       const championImg = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${championIcon}`;
-
-//       const divElement = document.createElement('div');
-//       divElement.classList.add("card");
-
-//       divElement.innerHTML = `
-//         <div class="card">
-//           <h3 class="card">${championName}</h3>
-//           <img class="card" src="${championImg}"/>
-//         </div>
-//       `;
-//       mainTag.appendChild(divElement);
-
-//       const champ = {
-//         name: championName,
-//         img: championImg
-//       }
-
-//       localStorage.setItem('champ', JSON.stringify(champ))
-//       const champFromLocalStorage = JSON.parse(localStorage.getItem('champ'));
-//       console.log(champFromLocalStorage.name)
-//     }
-//   });
-//   } catch (error) {
-//     console.error('Erro:', error);
-//   }
-// };
-
 
 const search = async (value) => {
   insertChamp(value);
@@ -105,7 +91,6 @@ const send = () => {
 }
 
 // Starting with Default
-
 fetchIsso();
 insertChamp('');
 localStorage.clear();
