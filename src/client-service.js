ed45c1292300942759c4a362e9cda02831affada
WagnerChr <wagnerckv@gmail.com>
2023-06-10 14:25:09 -0300

    refactor: Separete fetchChampion function, Add insertChamp function

--- src/client-service.js
+++ src/client-service.js
@@ -1,67 +1,112 @@
 // Configs
 const apiUrl = 'https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json';
-// const jsonServerUrl = 'http://localhost:3000/champions';
+let mainTag = document.querySelector('main');
 
-const fetchChampions = async (value) => {
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    const champions = Object.values(data.data);
-    let mainTag = document.querySelector('main');
-    mainTag.innerHTML = ''
-    
-    champions.forEach(champion => {
+// Starts App
+const fetchIsso = async () => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const champions = Object.values(data.data);
+
+  champions.forEach(champion => {
       const championName = champion.id;
-      const championIcon = champion.image.full;
+      const championIcon = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${champion.image.full}`;    
+      const champ = {
+        name: championName,
+        img: championIcon
+      };
 
-    if(championName.toLowerCase().startsWith(value.toLowerCase())) {
+      let key = localStorage.length + 1;
+      localStorage.setItem( key, JSON.stringify(champ));
+  });
+}
 
-      const championImg = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${championIcon}`;
+// Inserts Champions
+const insertChamp = (value) => {
+  mainTag.innerHTML = ''
+  for (let i = 1; i < localStorage.length; i++) {
+    champFromLocalStorage = JSON.parse(localStorage.getItem(i));
 
-      const divElement = document.createElement('div');
-      divElement.classList.add("card");
+  if(champFromLocalStorage.name.toLowerCase().startsWith(value.toLowerCase())) {
+    const divElement = document.createElement('div');
+    divElement.classList.add("card");
 
-      divElement.innerHTML = `
-        <div class="card">
-          <h3 class="card">${championName}</h3>
-          <img class="card" src="${championImg}"/>
-        </div>
-      `;
-      mainTag.appendChild(divElement);
-    }});
-  } catch (error) {
-    console.error('Erro:', error);
-  }
-};
+    divElement.innerHTML = `
+      <div class="card">
+        <h3 class="card">${champFromLocalStorage.name}</h3>
+        <img class="card" src="${champFromLocalStorage.img}"/>
+      </div>
+    `;
+    mainTag.appendChild(divElement);    
+  }}
+}
+
+
+// const fetchChampions = async (value) => {
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+//     const champions = Object.values(data.data);
+    
+//     mainTag.innerHTML = ''
+    
+//     champions.forEach(champion => {
+//       const championName = champion.id;
+//       const championIcon = champion.image.full;
+
+//     if(championName.toLowerCase().startsWith(value.toLowerCase())) {
+
+//       const championImg = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${championIcon}`;
+
+//       const divElement = document.createElement('div');
+//       divElement.classList.add("card");
+
+//       divElement.innerHTML = `
+//         <div class="card">
+//           <h3 class="card">${championName}</h3>
+//           <img class="card" src="${championImg}"/>
+//         </div>
+//       `;
+//       mainTag.appendChild(divElement);
+
+//       const champ = {
+//         name: championName,
+//         img: championImg
+//       }
+
+//       localStorage.setItem('champ', JSON.stringify(champ))
+//       const champFromLocalStorage = JSON.parse(localStorage.getItem('champ'));
+//       console.log(champFromLocalStorage.name)
+//     }
+//   });
+//   } catch (error) {
+//     console.error('Erro:', error);
+//   }
+// };
+
 
 const search = async (value) => {
-  await fetchChampions(value);
+  insertChamp(value);
 }
 
 const send = () => {
-
   const inputName = document.getElementById('input-name');
   const inputImg = document.getElementById('input-img');
-
   const valueName = inputName.value,
         valueImg = inputImg.value;
+  const champ = {
+    name: valueName,
+    img: valueImg
+  }  
+  let key = localStorage.length + 1
+  localStorage.setItem(key, JSON.stringify(champ));
 
-  const divElement = document.createElement('div');
-  divElement.innerHTML = `
-    <div class="card">
-      <h3 class="card">${valueName}</h3>
-      <img class="card" src="${valueImg}"/>
-    </div>
-  `
-  console.log(
-    valueName +
-    "\n- - -\n" +
-    valueImg
-  )
-
-  // Problem
-  // mainTag.appendChild(divElement)
+  insertChamp('');
 }
 
 // Starting with Default
-fetchChampions('');
+
+fetchIsso();
+insertChamp('');
+localStorage.clear();
+
