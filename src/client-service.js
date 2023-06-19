ecdb4ec3cc608da213f5e90fb403dca9bc363c2f
WagnerChr <wagnerckv@gmail.com>
2023-06-19 03:15:48 -0300

    feat: Add showInfo modal

--- src/client-service.js
+++ src/client-service.js
@@ -1,6 +1,11 @@
 // Configs
-const apiUrl = 'https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json';
+const apiUrl = 'http://ddragon.leagueoflegends.com/cdn/13.12.1/data/en_US/champion.json';
+
+// const apiChamp = `http://ddragon.leagueoflegends.com/cdn/img/champion/${champion}/`
+
 let cardList = document.querySelector('.card-list');
+
+
 
 // Starts App
 const fetchIsso = async () => {
@@ -10,7 +15,7 @@ const fetchIsso = async () => {
 
   champions.forEach(champion => {
       const championName = champion.id;
-      const championIcon = `https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${champion.image.full}`;    
+      const championIcon = `https://ddragon.leagueoflegends.com/cdn/13.12.1/img/champion/${champion.image.full}`;    
       
       let key = localStorage.length + 1;
 
@@ -22,6 +27,99 @@ const fetchIsso = async () => {
       localStorage.setItem( key, JSON.stringify(champ));
   });
 }
+
+const showInfo = async (id) => {
+
+  const champion = JSON.parse(localStorage.getItem(id));
+  const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/13.12.1/data/en_US/champion/${champion.name}.json`);
+  const data = await response.json();
+  const champ = Object.values(data.data);
+
+  const champName = champ[0].name;
+  const champSkins = champ[0].skins;
+
+  console.log(champSkins[0].num)
+  console.log(champ[0].name)
+
+
+  const carousel = document.querySelector('.carousel-wrapper');
+
+  // Create Spans
+  for(let n = 1; n <= champSkins.length; n++) {
+    let spanItem = document.createElement('span')
+    spanItem.id = `item-${n}`
+    
+    carousel.appendChild(spanItem)
+  }
+
+  // Create itens
+  for(let n = 1; n <= champSkins.length; n++) {
+    let carouselItem = document.createElement('div')
+    carouselItem.classList.add('carousel-item')
+    carouselItem.classList.add(`item-${n}`)
+    
+    carousel.appendChild(carouselItem)
+  }
+
+  //Create other itens
+  for(let n = 1; n <= champSkins.length; n++) {
+    let carouselItem = document.querySelector(`.item-${n}`)
+    
+      let aPrev = document.createElement('a')
+      let aNext = document.createElement('a')
+
+      aPrev.classList.add('arrow-prev', 'arrow')
+      aNext.classList.add('arrow-next', 'arrow')
+
+      aPrev.href = `#item-${n === 1 ? champSkins.length : n - 1}`;
+      aNext.href = `#item-${n === champSkins.length ? 1 : n + 1}`;
+
+      console.log(`#item-${n === 1 ? champSkins.length : n - 1}`)
+      console.log(`#item-${n === champSkins.length ? 1 : n + 1}`)
+
+      carouselItem.appendChild(aPrev)
+      carouselItem.appendChild(aNext)
+  }
+
+  // Setting champ images
+  for(let n = 0; n < champSkins.length; n++) {
+    let carouselItem = document.querySelector(`.item-${n + 1}`)
+
+    console.log 
+    carouselItem.style.backgroundImage = `url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champName}_${champSkins[n].num}.jpg')`;
+  }
+
+
+  
+  let modal = document.querySelector(".modal-info");
+  let imgDiv = document.querySelector(".champion-img-info");
+
+  // let imgElement = document.createElement("img"); 
+  // imgElement.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg`
+  // imgDiv.appendChild(imgElement)
+
+
+  let closeBtn = document.querySelector(".close");
+  modal.style.display = "flex";
+
+
+  // closeBtn.removeEventListener('click', closeBtn);
+  // closeBtn.addEventListener('click', () => {
+  //   modal.style.display = "none";
+  //   championImage.innerHTML = ''
+  //   champFromLocalStorage = ''
+  // }, { once: true });
+
+
+  
+  champFromLocalStorage = ''
+  id = ''
+}
+
+const fetchSplashs = async () => {
+  const response = await fetch(apiChamp)
+}
+
 
 const add = () => {
 
@@ -53,10 +151,10 @@ const add = () => {
   closeBtn.addEventListener('click', () => {
     modal.style.display = "none";
     championImage.innerHTML = ''
-  });
+    imgElement = ''
+  }, { once: true } );
 
   editBtn.removeEventListener('click', editBtn);
-
   editBtn.addEventListener('click', () => {
     let champName = inputName.value,
         champImg = inputImage.value
@@ -77,10 +175,8 @@ const add = () => {
 const send = (champ) => {
  
   console.log("Sim, recebo o champ: \n " + champ)
-
   let key = localStorage.length + 1
   localStorage.setItem(key, JSON.stringify(champ));
-
   insertChamp('');
 }
 
@@ -94,8 +190,9 @@ const edit = (id) => {
   let champFromLocalStorage = JSON.parse(localStorage.getItem(id));
 
   let editBtn = document.querySelector(".edit-btn");
-  editBtn.textContent = "Editar campeão"
+  let closeBtn = document.querySelector(".close");
 
+  editBtn.textContent = "Editar campeão"
 
   let inputName = document.querySelector(".input-edit-name")
   let inputImage = document.querySelector(".input-edit-img")
@@ -109,16 +206,16 @@ const edit = (id) => {
   imgElement.src=`${champFromLocalStorage.img}`
   championImage.appendChild(imgElement)
 
-  console.log("Valores: " + champFromLocalStorage)
 
-  let closeBtn = document.querySelector(".close");
+  closeBtn.removeEventListener('click', closeBtn);
   closeBtn.addEventListener('click', () => {
     modal.style.display = "none";
     championImage.innerHTML = ''
     champFromLocalStorage = ''
     id = ''
-  });
+  }, { once: true });
 
+  editBtn.removeEventListener('click', editBtn);
   editBtn.addEventListener('click', () => {
     let champFromLocalStorage2 = JSON.parse(localStorage.getItem(id));
 
@@ -131,8 +228,11 @@ const edit = (id) => {
     id = ''
 
     insertChamp('');
-  });
+  }, { once: true });
 }
+
+
+
 
 const remove = (id) => {
   console.log("Excluir ..." + id)
@@ -164,8 +264,12 @@ const insertChamp = (value) => {
         <div class="card"> 
           <div class="card-image">
             <img src="${champFromLocalStorage.img}"/>
+            <button class="infoButton">I</button>
+
             <button class="editButton">Edit</button>
             <button class="removeButton">Excluir</button>
+
+
           </div>
           <h3>${champFromLocalStorage.name}</h3>
         </div>
@@ -173,10 +277,15 @@ const insertChamp = (value) => {
 
     const editButton = cardElement.querySelector(".editButton");
     const removeButton = cardElement.querySelector(".removeButton");
+    const infoButton = cardElement.querySelector(".infoButton");
+
+
     const imgDiv = cardElement.querySelector(".card-image");
 
     editButton.addEventListener("click", edit.bind(null, i));
     removeButton.addEventListener("click", remove.bind(null, i));
+    infoButton.addEventListener("click", showInfo.bind(null, i));
+
 
     imgDiv.addEventListener("mouseenter", mouseEnter.bind(null, imgDiv));
 
@@ -187,7 +296,6 @@ const insertChamp = (value) => {
     }  
   }}
 }
-
 
 
 const search = async (value) => {
